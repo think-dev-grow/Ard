f86404f7c2d092d8dcfe5bea5bf3cb44cd829420
backend dev <developer@leapsail.com.ng>
2022-11-16 07:27:24 -0800

    wed  cpmmit 20

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,12 @@
 const express = require("express");
 
 const { sendOTP, verifyOTP, completeProfile } = require("../controllers/auth");
-const verifyToken = require("../utils/verifyToken");
 
 const router = express.Router();
 
 router.post("/send-otp", sendOTP);
 
-router.post("/verify-otp", verifyToken, verifyOTP);
+router.post("/verify-otp/:token", verifyOTP);
 
 router.post("/complete-profile/:id", completeProfile);
 
