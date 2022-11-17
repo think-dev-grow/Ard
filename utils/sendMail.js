ac10c1a0afa2d3e152d2d4c6061721b2e6379274
backend dev <developer@leapsail.com.ng>
2022-11-17 15:06:24 -0800

    thu commit 347

---
+++ utils/sendMail.js
@@ -0,0 +1,46 @@
+// https://i.postimg.cc/rmtXJNqX/illustration.png  girl
+//https://i.postimg.cc/wBWk35pJ/Logo-copy.png logo
+
+var { SendMailClient } = require("zeptomail");
+
+const url = "api.zeptomail.com/";
+const token =
+  "Zoho-enczapikey wSsVR61w/xT1Xah0lDD8Lr86kQhQA130EEV/31en4yL6Hf7Ep8dvlUHOBAauSKUYRDVsFTBHoOgpmxYHh2Bcidwpn1ACDiiF9mqRe1U4J3x17qnvhDzOX2VfkRuKLokAxghtn2hmGsEk+g==";
+
+let client = new SendMailClient({ url, token });
+
+const sendMail = (to, value) => {
+  client
+    .sendMail({
+      bounce_address: "NOREPLY@bounce.ardilla.africa",
+      from: {
+        address: "noreply@ardilla.africa",
+        name: "Ardilla",
+      },
+      to: [
+        {
+          email_address: {
+            address: `${to}`,
+            name: "",
+          },
+        },
+      ],
+      subject: "Email verifaction",
+      htmlbody: `<div style="text-align: center;">
+        <img src="https://ibb.co/CBWz5js" alt="" class="img-fluid" style="padding: 30px 0px;">
+        <hr>
+        <img src="https://ibb.co/S5wVMWn" alt="" class="img-fluid">
+        <h6 style="color: #041D05; font-size: 18px; font-weight: 500; line-height: 26px; font-family: 'Ubuntu'; margin-top: 20px;">Please use the OTP code below to complete your account setup:</h6>
+        <p style="color: #041D05; font-size: 58px; font-weight: 700; line-height: 76px; font-family: 'Ubuntu'; margin-top: 20px;">${value}</p>
+        <h5 style="color: #041D05; font-size: 17px; font-weight: 400; line-height: 26px; font-family: 'Ubuntu'; margin-top: 20px;">Or click the below link to verify your email address.</h5>
+        <a href="https://ardilla-web.netlify.app/otp">Click Here
+        </a>
+        <h3 style="color: #041D05; font-size: 19px; font-weight: 600; line-height: 26px; font-family: 'Ubuntu'; margin-top: 70px;">- The Ardilla Team</h3>
+        <small style="color: #041D05; font-size: 17px; font-weight: 500; line-height: 26px; font-family: 'Ubuntu'; margin-top: 20px;">Copyright © 2022 Ardilla. All rights reserved </small>
+      </div>`,
+    })
+    .then((resp) => console.log("success", resp))
+    .catch((error) => console.log("error", error));
+};
+
+module.exports = sendMail;
