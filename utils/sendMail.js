43e08eb61bd62cf16e4cb1f89be5f1107130c0b0
backend dev <developer@leapsail.com.ng>
2022-11-17 15:08:51 -0800

    thu commit 35678

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,6 +1,3 @@
-// https://i.postimg.cc/rmtXJNqX/illustration.png  girl
-//https://i.postimg.cc/wBWk35pJ/Logo-copy.png logo
-
 var { SendMailClient } = require("zeptomail");
 
 const url = "api.zeptomail.com/";
@@ -27,14 +24,12 @@ const sendMail = (to, value) => {
       ],
       subject: "Email verifaction",
       htmlbody: `<div style="text-align: center;">
-        <img src="https://ibb.co/CBWz5js" alt="" class="img-fluid" style="padding: 30px 0px;">
+        <img src="https://i.postimg.cc/wBWk35pJ/Logo-copy.png" alt="" class="img-fluid" style="padding: 30px 0px;">
         <hr>
-        <img src="https://ibb.co/S5wVMWn" alt="" class="img-fluid">
+        <img src="https://i.postimg.cc/rmtXJNqX/illustration.png " alt="" class="img-fluid">
         <h6 style="color: #041D05; font-size: 18px; font-weight: 500; line-height: 26px; font-family: 'Ubuntu'; margin-top: 20px;">Please use the OTP code below to complete your account setup:</h6>
         <p style="color: #041D05; font-size: 58px; font-weight: 700; line-height: 76px; font-family: 'Ubuntu'; margin-top: 20px;">${value}</p>
-        <h5 style="color: #041D05; font-size: 17px; font-weight: 400; line-height: 26px; font-family: 'Ubuntu'; margin-top: 20px;">Or click the below link to verify your email address.</h5>
-        <a href="https://ardilla-web.netlify.app/otp">Click Here
-        </a>
+        
         <h3 style="color: #041D05; font-size: 19px; font-weight: 600; line-height: 26px; font-family: 'Ubuntu'; margin-top: 70px;">- The Ardilla Team</h3>
         <small style="color: #041D05; font-size: 17px; font-weight: 500; line-height: 26px; font-family: 'Ubuntu'; margin-top: 20px;">Copyright © 2022 Ardilla. All rights reserved </small>
       </div>`,
